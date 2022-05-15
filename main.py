4d2290de6319a41a2dab2dd2e748c4cdabd662a2
roma <trueget33@gmail.com>
2022-05-16 00:26:22 +0300

    add file

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+c = random.randint(1,10)
+print('угадай число от 1 до 10!')
+while True:
+    n = input()
+    if str(c) == n:
+        print('Верно, это число - {c}')
+        break
+    else:
+        print('Попробуйте снова!')
